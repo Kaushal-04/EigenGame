bc48ae566544db1ac855d40e6bb582e9fd484eaa
Kaushal Kumar <133222412+Kaushal-04@users.noreply.github.com>
2024-06-22 16:58:22 +0530

    Change way to calculate Ax = nBx

--- EigenProject.cpp
+++ EigenProject.cpp
@@ -48,119 +48,155 @@ void deleteColumn(MatrixXf& matrix, int columnIndex) {
     }
     matrix.conservativeResize(matrix.rows(), matrix.cols() - 1);
 }
+float calculateSigma(const MatrixXf& matrix) {
+    EigenSolver<MatrixXf> eigenValueSolver(matrix);
+    MatrixXf eigValmatrix = (eigenValueSolver.eigenvalues()).real();
+    float maxAbsValue = eigValmatrix.cwiseAbs().maxCoeff();
+    return 1 / maxAbsValue;
+
+}
 MatrixXf eigenValues(const MatrixXf& eigenvectors) {
     SelfAdjointEigenSolver<MatrixXf> solver(eigenvectors.transpose() * eigenvectors);
     return solver.eigenvalues();
 }
-int main(){
+int main() {
     setValueOfn();
     srand(time(0));
-    MatrixXf FinalEigenVector(n,1);
+    MatrixXf finalEigVect(n,1);
     MatrixXf randomMatrixA , randomMatrixB;
     randomMatrixA.setRandom(n,n);
     randomMatrixA = randomMatrixA.array().abs(); // Ensure positive values
     randomMatrixB.setRandom(n,n);
     randomMatrixB = randomMatrixB.array().abs(); // Ensure positive values
-    MatrixXf symmetricMatrixA = makeSymmetric(randomMatrixA);
-    MatrixXf symmetricMatrixB = makeSymmetric(randomMatrixB);
-    cout << "Symmetric Matrix A:\n" << symmetricMatrixA << endl;
-    cout << "Symmetric Matrix B:\n" << symmetricMatrixB << endl;
-
-    //Create <float> , dynamic matrix   & Finding EigenValues and Eigen vectors
-    MatrixXcf eigenValMatA , eigenValMatB;
-    MatrixXcf eigenVectMatA , eigenVectMatB;
-    EigenSolver<MatrixXf> eigenValueSolverA(symmetricMatrixA);
-    EigenSolver<MatrixXf> eigenValueSolverB(symmetricMatrixB);
-    eigenValMatA=eigenValueSolverA.eigenvalues();
-    eigenValMatB=eigenValueSolverB.eigenvalues();
-    eigenVectMatA=eigenValueSolverA.eigenvectors();
-    eigenVectMatB=eigenValueSolverB.eigenvectors();
-
-    MatrixXf eigVectRealA = eigenVectMatA.real();
-    MatrixXf eigVectRealB = eigenVectMatB.real();
+    MatrixXf A = makeSymmetric(randomMatrixA);
+    MatrixXf B = makeSymmetric(randomMatrixB);
+          // Eigenvalue and Eigenvector Computation: The GeneralizedEigenSolver class in Eigen is used
+          // to solve the generalized eigenvalue problem Ax=λBx. The .compute(A, B) method computes the 
+          // eigenvalues and eigenvectors.
+    GeneralizedSelfAdjointEigenSolver<MatrixXf> ges(A , B);
+    MatrixXf eigenvalues = ges.eigenvalues();
+    MatrixXf eigenvectors = ges.eigenvectors();
+    for (int i = 0; i < eigenvectors.rows(); ++i) {
+        for (int j = 0; j < eigenvectors.cols(); ++j) {
+            eigenvectors(i, j) *= -1;
+        }
+    }
+    cout << "Eigenvalues: \n" << eigenvalues << endl;
+    cout << "Eigenvectors: \n" << eigenvectors << endl;
 
     MatrixXf EigenVector(n,1);
     MatrixXf temp;
-    for(int i=0; i<eigVectRealA.cols(); i++){
-        temp = normalizeVector(eigVectRealA.col(i));
-        insertColumnMatrix(EigenVector , temp);
-        deleteColumn(temp , 0);
-    }
-    for(int i=0; i<eigVectRealB.cols(); i++){
-        temp = normalizeVector(eigVectRealB.col(i));
+    for(int i=0; i<eigenvectors.cols(); i++){
+        temp = normalizeVector(eigenvectors.col(i));
         insertColumnMatrix(EigenVector , temp);
         deleteColumn(temp , 0);
     }
     deleteColumn(EigenVector , 0);
-   cout<<"Normalized Eigen Vector\n"<<EigenVector<<endl;
-
-
-    int T=3;  //just for checking otherwise set vale to 10001
-    MatrixXf Reward;
-    MatrixXf operation(n, n);
-    MatrixXf result;
+    cout<<"Normalized Eigen Vector\n"<<EigenVector<<endl;
+    MatrixXf w = B * EigenVector;
+    int T=1; //3 for just check
+    int M=2; //check it
+    vector<MatrixXf> matA;
+    MatrixXf tempMat;
+    for(int i=0; i<M; i++){
+        tempMat.setRandom(n,n);
+        tempMat = tempMat.array().abs(); // Ensure positive values
+        matA.push_back(tempMat);
+    }
+    vector<MatrixXf> matB;
+    for(int i=0; i<M; i++){
+        tempMat.setRandom(n,n);
+        tempMat = tempMat.array().abs(); // Ensure positive values
+        matB.push_back(tempMat);
+    }
+    MatrixXf Atm = MatrixXf::Zero(n,n);
+    MatrixXf Btm = MatrixXf::Zero(n,n);
     MatrixXf rewCala , rewCalaR , rewResl, rewCalb , rewCalbR , rewResR;
     float rewa , rewb;
+    MatrixXf Reward;
     int colNum;
     MatrixXf vi , vj;
- for (int j = 0; j < T; j++) { //T
-     for (int i = 0; i < n; i++) {  //n or k
+    for(int j=0; j<T; j++){
+        for(int i=0; i<n; i++){
+            MatrixXf Deltaim , DeltaBvim;
+            MatrixXf DelAvg(n,1) ;
+                for(int row=0;  row<n; row++){
+                    DelAvg(row , 0) = 0;
+                }
+            MatrixXf DelBvAvg(n,1) ;
+                for(int row=0;  row<n; row++){
+                    DelBvAvg(row , 0) = 0;
+                }
+            for(int m=0; m<M; m++){
+                Atm += matA[m];
+                Atm /= m+1;
+                Btm += matB[m];
+                Btm /= m+1;
+                colNum = rand()%EigenVector.cols();
+                vi = EigenVector.col(colNum);
+                colNum = rand()%EigenVector.cols();
+                vj = EigenVector.col(colNum);
 
-        colNum = rand()%EigenVector.cols();
-        vi = EigenVector.col(colNum);
-        colNum = rand()%EigenVector.cols();
-        vj = EigenVector.col(colNum);
-
-        float result = (vj.transpose() * symmetricMatrixB * vj)(0, 0);
-        if(result < 0) //to fin nan result
-            result = result * (-1);
-        float sqrtResult = sqrt(result);
-        sqrtResult = 1.0 /sqrtResult;
-        MatrixXf yj;
-        yj = vj / sqrtResult;
-        rewCala= vi.transpose() * symmetricMatrixB * vi;
-        rewa=rewCala(0,0);
-        rewCalaR=symmetricMatrixA * vi;
-        rewCalb=vi.transpose() * symmetricMatrixA * vi;
-        rewb=rewCalb(0,0);
-        rewCalbR=symmetricMatrixB * vi;
-        rewResl=rewCalaR * rewa;
-        rewResR=rewCalbR * rewb;
-        Reward=rewResl - rewResR;
-        if(j < i){
-            MatrixXf PenARes , PenBRes , diffRes;
-            MatrixXf Penalties(n,1) ;
-            for(int row=0;  row<n; row++){
-                Penalties(row , 0) = 0;
-            }
-            float PenAScal, PenBScal , PenCScal;
-            MatrixXf PenA , PenB , PenC , PenVecA , PenVecB;
-            PenA = vi.transpose() * symmetricMatrixA * yj;
-            PenAScal=PenA(0,0);
-            PenB = vi.transpose() * symmetricMatrixB * vi ;
-            PenBScal = PenB(0,0);
-            PenC = vi.transpose() * symmetricMatrixB * yj ;
-            PenCScal = PenC(0,0);
-            PenVecA = symmetricMatrixB * yj;
-            PenVecB = symmetricMatrixB * vi;
-            PenARes = PenBScal * PenVecA;
-            PenBRes = PenCScal * PenVecB;
-            diffRes = PenARes - PenBRes;
-            Penalties = Penalties + (PenAScal * diffRes);
-            MatrixXf delta(n,1);
-            delta = Reward - Penalties;
-            MatrixXf itadelta(n,1);
-            itadelta = -2 * delta;
-            MatrixXf wi(n,1);
-            wi = vi + itadelta;
-            vi = normalizeVector(wi);
-            insertColumnMatrix(FinalEigenVector , vi);
-         }
-     }
- }
-    deleteColumn(FinalEigenVector , 0);
-    cout<<"Final Eigen Vector:\n"<<FinalEigenVector<<endl;
-    MatrixXf EigenValues = eigenValues(FinalEigenVector);
+                float result = (vj.transpose() * B * vj)(0, 0);
+                if(result < 0) //to fin nan result
+                    result = result * (-1);
+                float sigma = calculateSigma(B);
+                float maxi = max(result , sigma);
+                float sqrtResult = sqrt(maxi);
+                sqrtResult = 1.0 /sqrtResult;
+                MatrixXf yj;
+                yj = vj * sqrtResult;
+                MatrixXf Byj = (B * vj )* sqrtResult;
+                //Calculate Reward
+                rewCala= vi.transpose() * Btm * vi;
+                rewa=rewCala(0,0);
+                rewCalaR = Atm * vi;
+                rewCalb = vi.transpose() * Atm * vi;
+                rewb = rewCalb(0,0);
+                rewCalbR = Btm * vi;
+                rewResl = rewCalaR * rewa;
+                rewResR = rewCalbR * rewb;
+                Reward = rewResl - rewResR;
+                //Calculate Penalties
+                int tempj = j;
+                MatrixXf Ba , Bb;
+                float BbL;
+                MatrixXf diffRes;
+                MatrixXf Penalties(n,1) ;
+                for(int row=0;  row<n; row++){
+                    Penalties(row , 0) = 0;
+                }
+                float PenA , PenB;
+                while(tempj < i){
+                    PenA = (vi.transpose() * Atm * yj)(0,0);
+                    PenB = (vi.transpose() * Btm * vi)(0,0) ;
+                    Ba = PenB * Byj ;
+                    BbL = (vi.transpose() * Byj)(0,0);
+                    Bb = (Btm * vi ) * BbL ;
+                    diffRes = Ba - Bb;
+                    Penalties = Penalties + (PenA * diffRes);
+                    tempj++;
+                }
+                Deltaim = Reward - Penalties;
+                DeltaBvim = (Btm * vi);
+                int randomCol = rand()%w.cols();
+                MatrixXf tempi = w.col(randomCol);
+                DeltaBvim -= tempi;
+                //Average of Deltaim
+                DelAvg += Deltaim;
+                DelBvAvg += DeltaBvim;
+            } 
+            DelAvg = DelAvg / M;      
+            MatrixXf newVi = vi + (-2 * DelAvg);
+            vi = normalizeVector(newVi);
+            insertColumnMatrix(finalEigVect , vi);
+            DelBvAvg = DelBvAvg / M;
+            w.col(i) = w.col(i) + (-2 * DelBvAvg);
+        }
+    }
+    deleteColumn(finalEigVect , 0);
+    cout<<"Final Eigen Vector\n"<<finalEigVect<<endl;
+    MatrixXf EigenValues = eigenValues(finalEigVect);
     cout<<"Eigen Values Matrix : \n"<<EigenValues<<endl;
     return 0;
 }
