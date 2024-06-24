63488d7d3ebdf5e66a0076fe8576c63fd016dad2
Kaushal Kumar <kaushaljee82@gmail.com>
2024-06-24 11:19:36 +0530

    Implemenatation of HPP file

--- HPP_FILE/EigenGame.cpp
+++ HPP_FILE/EigenGame.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include "EigenGame.hpp"
 
+using namespace Eigen;
+using namespace std;
+
+int main() {
+    MatrixXf A(3, 3);
+    A << 4, 1, 1,
+         1, 2, 3,
+         1, 3, 10;
+    displyMatrix(A);
+    return 0;
+}
---
+++ HPP_FILE/EigenGame.hpp
@@ -0,0 +1,11 @@
+#ifndef EIGENGAME_HPP
+#define EIGENGAME_HPP
+#include<Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+void displyMatrix(MatrixXf& matrix) {
+    cout << "Matrix\n" << matrix << endl;
+}
+
+#endif // EIGENGAME_HPP
